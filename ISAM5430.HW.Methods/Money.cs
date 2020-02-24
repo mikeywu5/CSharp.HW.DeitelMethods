5e7fa8d3c827cf9cead4be075fb67608df4f9350
Mike Y Wu <wum@uhcl.edu>
2020-02-24 11:14:43 -0600

    New HW

---
+++ ISAM5430.HW.Methods/Money.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharp.Assignments.Classes.Structure
+{
+    /// <summary>
+    /// Create a Money class that has data members dollars and cents.
+    ///   Include constructors that enable the Money class to be instantiated with a single value representing the full dollar/cent amount in decimal.
+    ///   Include a constructor that enables you to create an instance of the class by sending two separate integer values representing the dollar and cent amounts.
+    ///   Include another constructor that lets you initialize values from an existing Money object.
+    ///   Include IncrementMoney and DecrementMoney instance methods that take the Money instance to increment or decrement the money. 
+    ///   Include a public property named Dollars that gets and sets the value of the dollars. Dollars must not be negative; otherwise, throw an ArgumentOutOfRangeException.
+    ///   Include a public property named Cents that gets and sets the value of the cents. When cents are negative, subtract them from the Dollars and then make the cents positive again.
+    ///   Override the ToString( ) method to return the monetary amount formatted with currency symbols.
+    /// </summary>
+    public class Money
+    {
+    }
+}
---
+++ ISAM5430.HW.Methods/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CSharp.Assignments.Classes.Structure
+{
+    /// <summary>
+    /// Create a class representing a student.
+    ///   Include properties such as Student Number (string and read-only), First and Last Name (string and read-only),
+    ///     GPA (decimal: get and set), and Major (string: get and set).
+    ///   Write two constructors:
+    ///     the first initializing student number and first and last name
+    ///     while the other initializes all other fields in the same order.
+    ///   Include properties for each of the data items.
+    /// In the GPA's setter, ensure that the GPA is between 0 and 4; if it's out of range, throw an ArgumentOutOfRangeException.
+    /// </summary>
+    public class Student
+    {
+    }
+}
